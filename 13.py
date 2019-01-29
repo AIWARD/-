6e2f435afd549ebef0035b4cbe16635808346152
AIWARD <947223580@qq.com>
2019-01-29 17:42:43 +0800

    rename

--- 13.py
+++
@@ -1,4 +0,0 @@
-list = ['a','b','c','d','e','f','g','h']
-print("list为-----",list,"\nlist[::-1]",list[::-1])
-stri = "asmjhdkajsndaksndaksndkadsakjasjk"
-print("stri-----------------------",stri,'''\nstri.replace("ksndk","123")''',stri.replace("ksndk","123"))
